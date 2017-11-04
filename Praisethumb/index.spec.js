bccf398371d4980cc89573610300f31ef402155f
xiaochuan sun <xiaochuan sun>
2017-11-04 20:31:26 +0800

    demodemo

---
+++ Praisethumb/index.spec.js
@@ -0,0 +1,14 @@
+// describe('大拇指点赞测试用例',function () {
+// 	it('测试增加点赞',function () {
+// 		expect(window.add(1)).toBe(3);
+// 	});
+// });
+describe("A suite is just a function", function() {
+  var a;
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});
